59ac71211cac5d789d4e8508ee6d81aeab707291
Mykola <nyr3mvneov@gmail.com>
2026-01-05 02:37:41 +0200

    add classnames

--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import classNames from 'classnames';
 import { Todo } from '../../types/Todo';
 
 interface Props {
@@ -36,9 +37,9 @@ export const TodoList: React.FC<Props> = ({
           <tr
             key={todo.id}
             data-cy="todo"
-            className={
-              selectedTodoId === todo.id ? 'has-background-info-light' : ''
-            }
+            className={classNames({
+              'has-background-info-light': selectedTodoId === todo.id,
+            })}
           >
             <td className="is-vcentered">{todo.id}</td>
             <td className="is-vcentered">
@@ -50,9 +51,10 @@ export const TodoList: React.FC<Props> = ({
             </td>
             <td className="is-vcentered is-expanded">
               <p
-                className={
-                  todo.completed ? 'has-text-success' : 'has-text-danger'
-                }
+                className={classNames({
+                  'has-text-success': todo.completed,
+                  'has-text-danger': !todo.completed,
+                })}
               >
                 {todo.title}
               </p>
@@ -66,11 +68,10 @@ export const TodoList: React.FC<Props> = ({
               >
                 <span className="icon">
                   <i
-                    className={
-                      selectedTodoId === todo.id
-                        ? 'far fa-eye-slash'
-                        : 'far fa-eye'
-                    }
+                    className={classNames('far', {
+                      'fa-eye-slash': selectedTodoId === todo.id,
+                      'fa-eye': selectedTodoId !== todo.id,
+                    })}
                   />
                 </span>
               </button>
--- src/components/TodoModal/TodoModal.tsx
+++ src/components/TodoModal/TodoModal.tsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import classNames from 'classnames';
 import { Loader } from '../Loader';
 import { Todo } from '../../types/Todo';
 import { User } from '../../types/User';
@@ -52,11 +53,14 @@ export const TodoModal: React.FC<Props> = ({ todo, onClose }) => {
 
             {user && (
               <p className="block" data-cy="modal-user">
-                {todo.completed ? (
-                  <strong className="has-text-success">Done</strong>
-                ) : (
-                  <strong className="has-text-danger">Planned</strong>
-                )}
+                <strong
+                  className={classNames({
+                    'has-text-success': todo.completed,
+                    'has-text-danger': !todo.completed,
+                  })}
+                >
+                  {todo.completed ? 'Done' : 'Planned'}
+                </strong>
 
                 {' by '}
 
